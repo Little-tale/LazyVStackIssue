f63e32e53712d98de8a47cbbf15d499fc73173b2
Kim JaeHyung <you3192001@icloud.com>
2024-10-24 15:16:46 +0900

    Initial Commit

---
+++ LazyVStackIssue/LazyVStackIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVStackIssueApp.swift
+//  LazyVStackIssue
+//
+//  Created by Jae hyung Kim on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LazyVStackIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
